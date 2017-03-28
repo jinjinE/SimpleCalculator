d92ff9d0f877442bc1bc418cea4a6ac0cc8fa800
jinjinE <emirim0223@naver.com>
2017-03-28 20:14:09 +0900

    #2_2 Java Event Programing

--- app/src/main/java/emirim0223/kr/hs/emirim/simplecalculator/MainActivity.java
+++ app/src/main/java/emirim0223/kr/hs/emirim/simplecalculator/MainActivity.java
@@ -7,10 +7,11 @@ import android.widget.EditText;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/{
 
     EditText edit_first;
     EditText edit_second;
+    TextView text_result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +23,13 @@ public class MainActivity extends AppCompatActivity {
         Button but_minus = (Button)findViewById(R.id.but_minus);
         Button but_multiply = (Button)findViewById(R.id.but_multiply);
         Button but_division = (Button)findViewById(R.id.but_division);
-        final TextView text_result=(TextView)findViewById(R.id.text_result);
+        text_result=(TextView)findViewById(R.id.text_result);
+        but_plus.setOnClickListener(butHandler);
+        but_minus.setOnClickListener(butHandler);
+        but_multiply.setOnClickListener(butHandler);
+        but_division.setOnClickListener(butHandler);
 
-        but_plus.setOnClickListener(new View.OnClickListener() {
+        /*but_plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int num1=(Integer.parseInt(edit_first.getText().toString()) + Integer.parseInt(edit_second.getText().toString()));
@@ -51,6 +56,60 @@ public class MainActivity extends AppCompatActivity {
                 int num1=(Integer.parseInt(edit_first.getText().toString()) / Integer.parseInt(edit_second.getText().toString()));
                 text_result.setText(""+num1);
             }
-        });
-    }
+        });*/
+
+    }//end oncreat
+    View.OnClickListener butHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int num1=Integer.parseInt(edit_first.getText().toString());
+            int num2=Integer.parseInt(edit_second.getText().toString());
+            int result = 0;
+
+            switch(v.getId()){
+                case R.id.but_plus :
+                    result = num1 + num2;
+                    break;
+                case R.id.but_minus :
+                    result = num1 - num2;
+                    break;
+                case R.id.but_multiply :
+                    result = num1 * num2;
+                    break;
+                case R.id.but_division :
+                    result = num1 / num2;
+                    break;
+            }
+
+            text_result.setText(""+result);
+        }
+    };
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    /*@Override
+    public void onClick(View v) {
+        int num1=Integer.parseInt(edit_first.getText().toString());
+        int num2=Integer.parseInt(edit_second.getText().toString());
+        int result = 0;
+
+        switch(v.getId()){
+            case R.id.but_plus :
+                result = num1 + num2;
+                break;
+            case R.id.but_minus :
+                result = num1 - num2;
+                break;
+            case R.id.but_multiply :
+                result = num1 * num2;
+                break;
+            case R.id.but_division :
+                result = num1 / num2;
+                break;
+        }
+
+        text_result.setText(""+result);
+    }*/
 }
